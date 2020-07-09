41ba8a96e33856a6b0473833d4884740e72ebc3c
Vincent Viloria <vviloria01@gmail.com>
2020-07-09 13:33:59 -0700

    update color filtering for new valorant patch (1.03)

--- hsv_filter.py
+++ hsv_filter.py
@@ -5,23 +5,18 @@ import pyautogui
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
- 
-#thresholding
 def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 def get_bbox():
-	# image = cv2.imread("valorant_range_pic3.png")
+	# HSV bounds for current patch
+	lower_bound = [75, 30, 70]
+	upper_bound = [150, 90, 100]
+
 	image = np.array(pyautogui.screenshot())
-	hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-	# HSV bounds for gray area
-	lower_bound = [20, 3, 10]
-	upper_bound = [95, 50, 95]
-
+	bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+	# image = cv2.imread("images/valorant_range_pic4.png") # used for testing
+	hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
 	lower = np.array(lower_bound, dtype="uint8")
 	upper = np.array(upper_bound, dtype="uint8")
 	mask = cv2.inRange(hsv_image, lower, upper)
@@ -29,12 +24,16 @@ def get_bbox():
 	gray = get_grayscale(output)
 	thresh = thresholding(gray)
 	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	cnt = max(contours, key = cv2.contourArea)
+	cnt = max(contours, key=cv2.contourArea)
 	x,y,w,h = cv2.boundingRect(cnt)
-
 	full_bbox = (x, y, x+w, y+h)
 	score_bbox = (x, y, int(x + w / 2), y + h)
 	remain_bbox = (int(x + w / 2), y, x + w, y + h)
-	return (full_bbox, score_bbox, remain_bbox)
+	
+	# used for testing
+	# cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	# cv2.imshow('image', hsv_image)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
 
-
+	return full_bbox, score_bbox, remain_bbox
--- image_processing.py
+++ image_processing.py
@@ -1,47 +1,50 @@
 import cv2
 import numpy as np
 import pyautogui
-import detect_color
+import hsv_filter
 from PIL import Image, ImageGrab
 import pytesseract
-
-def get_inverted_img(image):
-    frame = np.array(image)
-    gray = get_grayscale(frame)
-    thresh = thresholding(gray)
-    inverted = invert(thresh)
-    return inverted
 
 def get_bbox_dims(bbox):
     return (bbox[0], bbox[1], bbox[2], bbox[3])
 
 def get_score_region():
-    """returns an image of the SCORE side of the RANGE scoreboard
-    Returns:
-        numpy array: image of SCORE side converted into np array;
-            contains the str 'SCORE' and score # (int)
-    """
-    full_bbox, score_bbox, remain_bbox = detect_color.get_bbox()
+    full_bbox, score_bbox, remain_bbox = hsv_filter.get_bbox()
     img = ImageGrab.grab(score_bbox)
     return np.array(img)
 
 def get_remain_region():
-    """returns an image of the SCORE side of the REMAINING scoreboard
-    Returns:
-        numpy array: image of REMAINING side converted into np array;
-            contains the str 'REMAINING' and score # (int)
-    """
-    full_bbox, score_bbox, remain_bbox = detect_color.get_bbox()
+    full_bbox, score_bbox, remain_bbox = hsv_filter.get_bbox()
     img = ImageGrab.grab(remain_bbox)
     return np.array(img)
 
 def get_full_region():
-    full_bbox, score_bbox, remain_bbox = detect_color.get_bbox()
+    full_bbox, score_bbox, remain_bbox = hsv_filter.get_bbox()
     img = ImageGrab.grab(full_bbox)
     return np.array(img)
 
+def get_processed_img(image):
+    frame = np.array(image)
+    gray = get_grayscale(frame)
+    thresh = thresholding(gray)
+    inverted = invert(thresh)
+    no_noise = remove_noise(inverted)
+    deskewed = deskew(no_noise)
+    return deskewed
+
+# preprocessing functions used are from https://bit.ly/2O8pQRT 
+
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def invert(image):
+    return cv2.bitwise_not(image)
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 7)
 
 def deskew(image):
     coords = np.column_stack(np.where(image > 0))
@@ -55,31 +58,3 @@ def deskew(image):
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
     return rotated
-
-#thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-def invert(image):
-    return cv2.bitwise_not(image)
-
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-
-def get_thresh_img(image):
-    frame = np.array(image)
-    gray = get_grayscale(frame)
-    thresh = thresholding(gray)
-    return thresh
-
-def get_resized_image(filename):
-    basewidth = 1000
-    img = Image.open(filename)
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img.save('RESIZED_IMAGE.jpg')
-    return cv2.imread('RESIZED_IMAGE.jpg')
